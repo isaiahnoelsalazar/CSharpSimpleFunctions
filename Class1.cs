1e50c8cb1ce761eaa72c83910d9a38f5bb0cf800
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-11 01:16:52 +0800

    new: add TestMethod2() in Class1

--- Class1.cs
+++ Class1.cs
@@ -8,5 +8,10 @@ namespace CSharpSimpleFunctions
         {
             Console.WriteLine("This is a test method in C# 7.3");
         }
+
+        public void TestMethod2()
+        {
+            Console.WriteLine("This is a test method #2 in C# 7.3");
+        }
     }
 }
